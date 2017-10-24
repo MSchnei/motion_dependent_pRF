1d37eb4394717ebe8b5ce65cbdd7d402df38adaf
MSchnei <marian.schneider@maastrichtuniversity.nl>
2017-10-24 23:07:24 +0200

    added wikipedia raised cosine implementation.

---
+++ raisedCosineFunction.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 24 22:40:50 2017
+
+@author: Marian
+"""
+
+import numpy as np
+
+
+T = 0.5
+steps = 50
+beta = 0.5
+
+
+def raisedCos(steps, T=0.5, beta=0.5):
+    """"Create binary wedge-and-ring mask.
+    Parameters
+    ----------
+    steps : float
+        Number of points in the output window
+    T: float
+        The symbol-period
+    beta : float
+        Roll-off factor
+    Returns
+    -------
+    hf : 1d np.array
+        Raised-cosine filter in frequency space
+    """
+
+    frequencies = np.linspace(-1/T, 1/T, steps)
+    
+    hf = np.empty(len(frequencies))
+    for ind, f in enumerate(frequencies):
+        if np.less_equal(np.abs(f), (1-beta)/(2*T)):
+            hf[ind] = 1
+        elif np.logical_and(np.less_equal(np.abs(f), (1+beta)/(2*T)),
+                            np.greater(np.abs(f), (1-beta)/(2*T))):
+            hf[ind] = 0.5*(1+np.cos((np.pi*T/2)*(np.abs(f)-(1-beta)/2*T)))
+        
+        else:
+            hf[ind] = 0
+    return hf
+
+window = raisedCos(60, T=1, beta=0.3)
