dd5b5d00de7783d585e6a15658a0c769ca43de84
MSchnei <marian.schneider@maastrichtuniversity.nl>
2017-11-06 13:14:17 +0100

    Added config files.

---
+++ Code/config_MotDepPrf.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""Define stimulus parameters for motion dependent pRF"""
+
+# %% general stimulus parameters
+
+# number of expected frames per second
+nFrames = 60.
+# height of the entire field of view in deg of vis angle
+fovHeight = 11.
+# number of pixels for the entire filed of view
+pix = 1024.
+# size of the bar aperture in deg of vis angle
+barSize = 1.7
+# by how much will the aperture step through visual field? in deg of vis angle
+stepSize = 0.34
+
+# %% set parameters for the texture
+
+# define the spatial frequency of the radial sine wave grating
+spatFreq = 2.
+# define the number of angular cycles of the radial sine wave grating
+angularCycles = 32.
+# number of cycles per deg of vis angle per second
+cycPerSec = 2.5
