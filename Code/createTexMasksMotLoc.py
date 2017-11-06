0da42e284222168e6eca0c8d0bd8450c5a82559b
MSchnei <marian.schneider@maastrichtuniversity.nl>
2017-11-06 14:23:35 +0100

    Integration config file.

--- Code/createTexMasksMotLoc.py
+++ Code/createTexMasksMotLoc.py
@@ -6,41 +6,18 @@ Script to creature textures and masks for the motion localiser.
 from __future__ import division  # so that 1/3=0.333 instead of 1/3=0
 import os
 import numpy as np
+import config_MotLoc as cfg
 from utils import carrierPattern, createBinCircleMask
 
-# %% set parameters
-
-# set the height of the field of view in deg
-fovHeight = 24.
-# set the number of frames
-nFrames = 60.
-# set the dimension (numbe rof pixels for the carrier pattern)
-pix = 1024
-
-# set the phase of the carrier pattern
-phase = np.linspace(0., 4.*np.pi, nFrames)
-lamb = np.sin(phase)/4. + 0.5
-
-# set the number of squares that the carrier pattern should be made up of
-# for the bars
-numSquaresBars = 8
-# set the number of squares that the carrier pattern should be made up of
-# for the wedge
-numSquaresWedge = 8
-
-# set the number of steps that the wedge should take
-wedgeSteps = 32
-# set the width of the width in deg
-wedgeWidth = 45
-# set the minimum radius fo the wedge
-minR = 0.4
 
 # %% create the carrier pattern (for the bars)
-barTexture = np.zeros((pix, pix, nFrames))
-
+barTexture = np.zeros((cfg.pix, cfg.pix, cfg.nFrames))
+# set the phase of the carrier pattern
+phase = np.linspace(0., 4.*np.pi, cfg.nFrames)
+lamb = np.sin(phase)/4. + 0.5
 for ind, (t, d) in enumerate(zip(phase, lamb)):
 
-    ima = carrierPattern(d, t, numSquaresBars, pix)
+    ima = carrierPattern(d, t, cfg.numSquaresBars, cfg.pix)
     # 1 = white, #-1 = black
     ima[np.greater(ima, 0)] = 1
     ima[np.less_equal(ima, 0)] = -1
@@ -57,25 +34,25 @@ for ind in np.arange(barTexture.shape[2]):
 horiBar = np.zeros((barTexture.shape))
 vertiBar = np.zeros((barTexture.shape))
 
-horiBar[0:pix/numSquaresBars, :, :] = np.copy(
-    barTexture[0:pix/numSquaresBars, :, :])
-vertiBar[:, 0:pix/numSquaresBars, :] = np.copy(
-    barTexture[:, 0:pix/numSquaresBars, :])
+horiBar[0:cfg.pix/cfg.numSquaresBars, :, :] = np.copy(
+    barTexture[0:cfg.pix/cfg.numSquaresBars, :, :])
+vertiBar[:, 0:cfg.pix/cfg.numSquaresBars, :] = np.copy(
+    barTexture[:, 0:cfg.pix/cfg.numSquaresBars, :])
 
 # %% create masks (for the bars)
 horiBarMask = np.zeros((barTexture.shape[:2]))
 vertiBarMask = np.zeros((barTexture.shape[:2]))
-horiBarMask[0:pix/numSquaresBars, :] = 1
+horiBarMask[0:cfg.pix/cfg.numSquaresBars, :] = 1
 horiBarMask = horiBarMask * 2 - 1
-vertiBarMask[:, 0:pix/numSquaresBars] = 1
+vertiBarMask[:, 0:cfg.pix/cfg.numSquaresBars] = 1
 vertiBarMask = vertiBarMask * 2 - 1
 
 # %% create the carrier pattern (for the wedge)
-wedgeTexture = np.zeros((pix, pix, nFrames))
+wedgeTexture = np.zeros((cfg.pix, cfg.pix, cfg.nFrames))
 
 for ind, (t, d) in enumerate(zip(phase, lamb)):
 
-    ima = carrierPattern(d, t, numSquaresWedge, pix)
+    ima = carrierPattern(d, t, cfg.numSquaresWedge, cfg.pix)
     # 1 = white, #-1 = black
     ima[np.greater(ima, 0)] = 1
     ima[np.less_equal(ima, 0)] = -1
@@ -84,20 +61,20 @@ for ind, (t, d) in enumerate(zip(phase, lamb)):
 # %% create the masks for the wedge
 
 # derive the angles for the wedge limits
-minTheta = np.linspace(0, 360, wedgeSteps, endpoint=False)
-maxTheta = minTheta + wedgeWidth
+minTheta = np.linspace(0, 360, cfg.wedgeSteps, endpoint=False)
+maxTheta = minTheta + cfg.wedgeWidth
 # get combinations of angles and radis
-combis = zip(np.zeros(len(minTheta)), np.ones(len(minTheta))*fovHeight/2.,
+combis = zip(np.zeros(len(minTheta)), np.ones(len(minTheta))*cfg.fovHeight/2.,
              minTheta, maxTheta)
 # create the wedgeMasks
-wedgeMasks = np.empty((pix, pix, len(combis)), dtype='int32')
+wedgeMasks = np.empty((cfg.pix, cfg.pix, len(combis)), dtype='int32')
 for ind, combi in enumerate(combis):
-    wedgeMasks[..., ind] = createBinCircleMask(fovHeight, pix,
+    wedgeMasks[..., ind] = createBinCircleMask(cfg.fovHeight, cfg.pix,
                                                rLow=combi[0],
                                                rUp=combi[1],
                                                thetaMin=combi[2],
                                                thetaMax=combi[3],
-                                               rMin=minR)
+                                               rMin=cfg.minR)
 wedgeMasks = wedgeMasks*2-1
 
 # %%  save textures (for the wedge)
