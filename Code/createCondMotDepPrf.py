407d74f90e0c336462e9fc90ce66bd715a60b938
MSchnei <marian.schneider@maastrichtuniversity.nl>
2017-11-07 18:04:53 +0100

    Adjust presentation order. Create counterbalanced runs

--- Code/createCondMotDepPrf.py
+++ Code/createCondMotDepPrf.py
@@ -7,6 +7,7 @@ Created on Thu Jun 23 16:21:45 2016
 
 from __future__ import division  # so that 1/3=0.333 instead of 1/3=0
 import numpy as np
+import itertools
 import os
 
 # %% set paramters
@@ -23,10 +24,10 @@ NrNullTrialStart = 1  # 12
 NrNullTrialEnd = 12
 NrNullTrialBetw = 12
 
+# %% prepare
 
 # prepare vector for motion direction; define 8 directions of motion
 BarApertures = np.linspace(1, NrOfApertures, NrOfApertures)
-
 
 # prepare vector for presentation order
 presOrder1 = np.hstack((np.zeros(NrNullTrialStart),
@@ -47,52 +48,67 @@ presOrder2 = np.hstack((np.zeros(NrNullTrialStart),
                         np.zeros(NrNullTrialEnd),
                         ))
 
-# prepare to indicate weather horizontal, vertical bar apeture or wedge
-aperture = np.hstack((np.zeros(NrNullTrialStart),
-                      np.ones(len(BarApertures))*1,
-                      np.zeros(NrNullTrialBetw),
-                      np.ones(len(BarApertures))*2,
-                      np.zeros(NrNullTrialBetw),
-                      np.ones(len(BarApertures))*3,
-                      np.zeros(NrNullTrialEnd),
-                      ))
+# prepare to indicate weather flicker, expanding or contracting motion
+# counterbalance occurance of
+lst = list(itertools.permutations(np.arange(NrOfCond)+1, NrOfCond))
+for ind, item in enumerate(lst):
+    lst[ind] = list(np.hstack(item))
+aryPerm = np.array(lst)
 
-# concatenate presOrder and aperture
-Conditions1 = np.vstack((presOrder1, aperture)).T
-Conditions2 = np.vstack((presOrder2, aperture)).T
+# loop through combinations
+for ind, indCond in enumerate(aryPerm):
+    aperture = np.hstack((np.zeros(NrNullTrialStart),
+                          np.ones(NrOfApertures)*indCond[0],
+                          np.zeros(NrNullTrialBetw),
+                          np.ones(NrOfApertures)*indCond[1],
+                          np.zeros(NrNullTrialBetw),
+                          np.ones(NrOfApertures)*indCond[2],
+                          np.zeros(NrNullTrialEnd),
+                          ))
 
+    # concatenate presOrder and aperture
+    Conditions1 = np.vstack((presOrder1, aperture)).T
+    Conditions2 = np.vstack((presOrder2, aperture)).T
 
-# %% Prepare target times
+    # %% Prepare target times
 
-# prepare targets
-NrOfTargets = int(len(Conditions1)/6)
-Targets = np.zeros(len(Conditions1))
-lgcRep = True
-while lgcRep:
-    TargetPos = np.random.choice(np.arange(NrNullTrialStart,
-                                 len(Conditions1)-NrNullTrialEnd), NrOfTargets,
-                                 replace=False)
-    lgcRep = np.greater(np.sum(np.diff(np.sort(TargetPos)) == 1), 0)
-Targets[TargetPos] = 1
-assert NrOfTargets == np.sum(Targets)
+    # prepare targets
+    NrOfTargets = int(len(Conditions1)/6)
+    Targets = np.zeros(len(Conditions1))
+    lgcRep = True
+    while lgcRep:
+        TargetPos = np.random.choice(np.arange(NrNullTrialStart,
+                                     len(Conditions1)-NrNullTrialEnd),
+                                     NrOfTargets,
+                                     replace=False)
+        lgcRep = np.greater(np.sum(np.diff(np.sort(TargetPos)) == 1), 0)
+    Targets[TargetPos] = 1
+    assert NrOfTargets == np.sum(Targets)
 
-# prepare random target onset delay
-TargetOnsetinSec = np.random.uniform(0.1,
-                                     ExpectedTR-TargetDuration,
-                                     size=NrOfTargets)
+    # prepare random target onset delay
+    TargetOnsetinSec = np.random.uniform(0.1,
+                                         ExpectedTR-TargetDuration,
+                                         size=NrOfTargets)
 
-# %% save the results
+    # %% save the results
 
-str_path_parent_up = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), '..'))
-filename1 = os.path.join(str_path_parent_up, 'Conditions',
-                         'Conditions_MotDepPrf_run01')
-np.savez(filename1, Conditions1=Conditions1,
-         TargetTRs=Targets, TargetOnsetinSec=TargetOnsetinSec,
-         TargetDuration=TargetDuration, ExpectedTR=ExpectedTR)
-
-filename2 = os.path.join(str_path_parent_up, 'Conditions',
-                         'Conditions_MotDepPrf_run02')
-np.savez(filename2, Conditions2=Conditions2,
-         TargetTRs=Targets, TargetOnsetinSec=TargetOnsetinSec,
-         TargetDuration=TargetDuration, ExpectedTR=ExpectedTR)
+    str_path_parent_up = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), '..'))
+    if 2*ind+1 > 9:
+        filename1 = os.path.join(str_path_parent_up, 'Conditions',
+                                 'Conditions_MotDepPrf_run' + str(2*ind+1))
+    else:
+        filename1 = os.path.join(str_path_parent_up, 'Conditions',
+                                 'Conditions_MotDepPrf_run0' + str(2*ind+1))
+    np.savez(filename1, Conditions=Conditions1,
+             TargetTRs=Targets, TargetOnsetinSec=TargetOnsetinSec,
+             TargetDuration=TargetDuration, ExpectedTR=ExpectedTR)
+    if 2*ind+2 > 9:
+        filename2 = os.path.join(str_path_parent_up, 'Conditions',
+                                 'Conditions_MotDepPrf_run' + str(2*ind+2))
+    else:
+        filename2 = os.path.join(str_path_parent_up, 'Conditions',
+                                 'Conditions_MotDepPrf_run0' + str(2*ind+2))
+    np.savez(filename2, Conditions=Conditions2,
+             TargetTRs=Targets, TargetOnsetinSec=TargetOnsetinSec,
+             TargetDuration=TargetDuration, ExpectedTR=ExpectedTR)
--- Code/createCondMotLoc.py
+++ Code/createCondMotLoc.py
@@ -8,6 +8,7 @@ Created on Thu Jun 23 16:21:45 2016
 from __future__ import division  # so that 1/3=0.333 instead of 1/3=0
 import numpy as np
 import os
+import itertools
 
 # %% set paramters
 NrOfBarApertures = 43
@@ -49,52 +50,66 @@ presOrder2 = np.hstack((np.zeros(NrNullTrialStart),
                         np.zeros(NrNullTrialEnd),
                         ))
 
-# prepare to indicate weather horizontal, vertical bar apeture or wedge
-aperture = np.hstack((np.zeros(NrNullTrialStart),
-                      np.ones(2*len(BarApertures))*1,
-                      np.zeros(NrNullTrialBetw),
-                      np.ones(2*len(BarApertures))*2,
-                      np.zeros(NrNullTrialBetw),
-                      np.ones(2*len(WedgeApertures))*3,
-                      np.zeros(NrNullTrialEnd),
-                      ))
+# prepare to indicate weather flicker, expanding or contracting motion
+# counterbalance occurance of
+lst = list(itertools.permutations(np.arange(NrOfCond)+1, NrOfCond))
+for ind, item in enumerate(lst):
+    lst[ind] = list(np.hstack(item))
+aryPerm = np.array(lst)
 
-# concatenate presOrder and aperture
-Conditions1 = np.vstack((presOrder1, aperture)).T
-Conditions2 = np.vstack((presOrder2, aperture)).T
+# loop through combinations
+for ind, indCond in enumerate(aryPerm):
+    aperture = np.hstack((np.zeros(NrNullTrialStart),
+                          np.ones(2*len(BarApertures))*indCond[0],
+                          np.zeros(NrNullTrialBetw),
+                          np.ones(2*len(BarApertures))*indCond[1],
+                          np.zeros(NrNullTrialBetw),
+                          np.ones(2*len(WedgeApertures))*indCond[2],
+                          np.zeros(NrNullTrialEnd),
+                          ))
 
+    # concatenate presOrder and aperture
+    Conditions1 = np.vstack((presOrder1, aperture)).T
+    Conditions2 = np.vstack((presOrder2, aperture)).T
 
-# %% Prepare target times
+    # %% Prepare target times
 
-# prepare targets
-NrOfTargets = int(len(Conditions1)/6)
-Targets = np.zeros(len(Conditions1))
-lgcRep = True
-while lgcRep:
-    TargetPos = np.random.choice(np.arange(NrNullTrialStart,
-                                 len(Conditions1)-NrNullTrialEnd), NrOfTargets,
-                                 replace=False)
-    lgcRep = np.greater(np.sum(np.diff(np.sort(TargetPos)) == 1), 0)
-Targets[TargetPos] = 1
-assert NrOfTargets == np.sum(Targets)
+    # prepare targets
+    NrOfTargets = int(len(Conditions1)/6)
+    Targets = np.zeros(len(Conditions1))
+    lgcRep = True
+    while lgcRep:
+        TargetPos = np.random.choice(np.arange(NrNullTrialStart,
+                                     len(Conditions1)-NrNullTrialEnd),
+                                     NrOfTargets,
+                                     replace=False)
+        lgcRep = np.greater(np.sum(np.diff(np.sort(TargetPos)) == 1), 0)
+    Targets[TargetPos] = 1
+    assert NrOfTargets == np.sum(Targets)
 
-# prepare random target onset delay
-TargetOnsetinSec = np.random.uniform(0.1,
-                                     ExpectedTR-TargetDuration,
-                                     size=NrOfTargets)
+    # prepare random target onset delay
+    TargetOnsetinSec = np.random.uniform(0.1,
+                                         ExpectedTR-TargetDuration,
+                                         size=NrOfTargets)
 
-# %% save the results
-
-str_path_parent_up = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), '..'))
-filename1 = os.path.join(str_path_parent_up, 'Conditions',
-                         'Conditions_MotLoc_run01')
-np.savez(filename1, Conditions=Conditions1,
-         TargetTRs=Targets, TargetOnsetinSec=TargetOnsetinSec,
-         TargetDuration=TargetDuration, ExpectedTR=ExpectedTR)
-
-filename2 = os.path.join(str_path_parent_up, 'Conditions',
-                         'Conditions_MotLoc_run02')
-np.savez(filename2, Conditions=Conditions2,
-         TargetTRs=Targets, TargetOnsetinSec=TargetOnsetinSec,
-         TargetDuration=TargetDuration, ExpectedTR=ExpectedTR)
+    # %% save the results
+    str_path_parent_up = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), '..'))
+    if 2*ind+1 > 9:
+        filename1 = os.path.join(str_path_parent_up, 'Conditions',
+                                 'Conditions_MotLoc_run' + str(2*ind+1))
+    else:
+        filename1 = os.path.join(str_path_parent_up, 'Conditions',
+                                 'Conditions_MotLoc_run0' + str(2*ind+1))
+    np.savez(filename1, Conditions=Conditions1,
+             TargetTRs=Targets, TargetOnsetinSec=TargetOnsetinSec,
+             TargetDuration=TargetDuration, ExpectedTR=ExpectedTR)
+    if 2*ind+2 > 9:
+        filename2 = os.path.join(str_path_parent_up, 'Conditions',
+                                 'Conditions_MotLoc_run' + str(2*ind+2))
+    else:
+        filename2 = os.path.join(str_path_parent_up, 'Conditions',
+                                 'Conditions_MotLoc_run0' + str(2*ind+2))
+    np.savez(filename2, Conditions=Conditions2,
+             TargetTRs=Targets, TargetOnsetinSec=TargetOnsetinSec,
+             TargetDuration=TargetDuration, ExpectedTR=ExpectedTR)
