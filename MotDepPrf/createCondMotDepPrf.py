cde3229cd7d01834fbc29f1e006c1b68a20c7359
MSchnei <marian.schneider@maastrichtuniversity.nl>
2018-07-03 18:29:21 +0200

    WIP implementing condition creation minimizing correlation.

--- MotDepPrf/createCondMotDepPrf.py
+++ MotDepPrf/createCondMotDepPrf.py
@@ -10,6 +10,7 @@ import os
 import numpy as np
 import config_MotDepPrf as cfg
 from utils import arrangePresOrder, arrangeHyperCondOrder, prepareTargets
+from scipy.stats import gamma
 
 # %% set paramters
 
@@ -35,23 +36,108 @@ nrNullTrialBetw = 14
 # set trial distance
 trialDist = 2
 
-# %% prepare presentation order for hyper conditions and targets
+# %% prepare presentation order for apetrtures and hyper conditions
 
 # get hyper conditions order, which here means
 lstHyperCond = arrangeHyperCondOrder(nrOfCond, nrNullTrialStart,
                                      nrNullTrialBetw, nrNullTrialEnd,
                                      nrOfApertures, cfg.numRep)
 
-# get target times
-targets, targetType = prepareTargets(len(lstHyperCond[0]), expectedTR,
-                                     targetDuration, targetDist)
-
-# %%
 # get presentation order of apertures
 presOrder = arrangePresOrder(nrOfCond, nrNullTrialStart, nrNullTrialBetw,
                              nrNullTrialEnd, nrOfApertures, cfg.numRep,
                              trialDist)
 
+nrOfHyperCombis = len(lstHyperCond)
+
+# %% concatenating presOrder and hyperCondOrder
+lstCnd = []
+for ind in np.arange(nrRuns):
+
+    # get hyper condition order
+    hyperCondOrder = lstHyperCond[ind % nrOfHyperCombis]
+    # create conditions by concatenating presOrder and hyperCondOrder
+    conditions = np.vstack((presOrder, hyperCondOrder)).T
+    # add to a list
+    lstCnd.append(conditions)
+
+condAllRuns = np.vstack(lstCnd)
+
+# %% Remap conditions into space of continuous, unique cond numbers
+
+# arySptCond is remapped into into space of continuous, unique cond numbers
+condRmp = np.empty((len(condAllRuns),))
+
+# get the condition nr
+condRmp = condAllRuns[:, 0] + condAllRuns[:, 1] * np.max(condAllRuns[:, 0])
+
+# get remapping to continuous numbers
+aryFrm = np.unique(condRmp)
+aryTo = np.argsort(np.unique(condRmp))
+
+# apply mapping
+condRmp = np.array(
+    [aryTo[aryFrm == i][0] for i in condRmp])
+
+# %% create neural responses
+uniqueCond = np.unique(condRmp)
+uniqueCond = uniqueCond[uniqueCond > 0]
+
+nrlTc = np.zeros((len(uniqueCond), len(condRmp)))
+for ind, uniCond in enumerate(uniqueCond):
+    indPos = np.where(condRmp == uniCond)[0]
+    nrlTc[ind, indPos] = 1
+
+
+# %% convolve with hrf function
+def funcHrf(varNumVol, varTr):
+    """Create double gamma function.
+
+    Source:
+    http://www.jarrodmillman.com/rcsds/lectures/convolution_background.html
+    """
+    vecX = np.arange(0, varNumVol, 1)
+
+    # Expected time of peak of HRF [s]:
+    varHrfPeak = 6.0 / varTr
+    # Expected time of undershoot of HRF [s]:
+    varHrfUndr = 12.0 / varTr
+    # Scaling factor undershoot (relative to peak):
+    varSclUndr = 0.35
+
+    # Gamma pdf for the peak
+    vecHrfPeak = gamma.pdf(vecX, varHrfPeak)
+    # Gamma pdf for the undershoot
+    vecHrfUndr = gamma.pdf(vecX, varHrfUndr)
+    # Combine them
+    vecHrf = vecHrfPeak - varSclUndr * vecHrfUndr
+
+    # Scale maximum of HRF to 1.0:
+    vecHrf = np.divide(vecHrf, np.max(vecHrf))
+
+    return vecHrf
+
+# create canonical hrf response
+vecHrf = funcHrf(nrlTc.shape[1], expectedTR)
+
+# prepare arrays for convolution by zeropadding
+nrlTcCnvl = np.zeros(nrlTc.shape)
+
+vecHrf = np.append(vecHrf, np.zeros(100))
+nrlTc = np.concatenate((nrlTc, np.zeros((nrlTc.shape[0], 100))), axis=1)
+
+# Convolve design matrix with HRF model:
+for ind, tc in enumerate(nrlTc):
+    nrlTcCnvl[ind, :] = np.convolve(tc, vecHrf,
+                                    mode='full')[:nrlTc.shape[1]-100]
+
+# %% correlation module
+targetCorr = 0.1
+corrSwitch = True
+while corrSwitch:
+    corrMatrix = np.corrcoef(nrlTcCnvl)
+    corrMatrixHalf = corrMatrix[np.triu_indices_from(corrMatrix, k=1)]
+    corrSwitch = np.invert(np.all(np.less(corrMatrixHalf, targetCorr)))
 
 
 # %% save the results
@@ -77,11 +163,10 @@ for ind in np.arange(nrRuns):
              targetDuration=targetDuration, targetType=targetType,
              expectedTR=expectedTR)
 
-# %% correlation module
-targetCorr = 0.1
-corrSwitch = True
-while corrSwitch:
-    timeCourses = np.random.random((3*60, 222*12))
-    corrMatrix = np.corrcoef(timeCourses)
-    corrMatrixHalf = corrMatrix[np.triu_indices_from(corrMatrix, k=1)]
-    corrSwitch = np.invert(np.all(np.less(corrMatrixHalf, targetCorr)))
+
+# get target times
+targets, targetType = prepareTargets(len(lstHyperCond[0]), expectedTR,
+                                     targetDuration, targetDist)
+
+
+
